a0d07e839ec8de217ae6d9a8f6a982c40dd1ac5f
P S Solanki <pssolanki111pss@gmail.com>
2021-09-15 00:20:16 +0530

    last quote, daily ochlv and aggregates tests written and working

--- tests/stocks_tests.py
+++ tests/stocks_tests.py
@@ -112,6 +112,87 @@ class TestStocks(unittest.TestCase):
         self.assertIsInstance(data, dict)
         self.assertEqual(data['status'], 'OK')
 
+    def test_get_last_quote(self):
+        with polygon.PolygonClient(cred.KEY) as client:
+            data = client.get_last_quote('AMD')
+            data1 = client.get_last_quote('AMD', raw_response=True)
+            data2 = client.get_last_quote('AMD', raw_response=True)
+
+            self.assertIsInstance(data, dict)
+            self.assertIsInstance(data1, Response)
+            self.assertIsInstance(data2, Response)
+
+            self.assertIsInstance(data1.json(), dict)
+            self.assertIsInstance(data2.json(), dict)
+
+            self.assertEqual(data['status'], 'OK')
+            self.assertEqual(data1.json()['status'], 'OK')
+            self.assertEqual(data2.json()['status'], 'OK')
+
+        # Testing without Context Manager
+        client = polygon.PolygonClient(cred.KEY)
+        data = client.get_last_quote('AMD')
+        client.close()
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data['status'], 'OK')
+
+    def test_get_daily_open_close(self):
+        with polygon.PolygonClient(cred.KEY) as client:
+            data = client.get_daily_open_close('AMD', '2021-06-28')
+            data1 = client.get_daily_open_close('AMD', datetime.datetime(2021, 6, 28), raw_response=True)
+            data2 = client.get_daily_open_close('AMD', datetime.datetime(2021, 6, 28), raw_response=True, adjusted=
+                                                False)
+
+            self.assertIsInstance(data, dict)
+            self.assertIsInstance(data1, Response)
+            self.assertIsInstance(data2, Response)
+
+            self.assertIsInstance(data1.json(), dict)
+            self.assertIsInstance(data2.json(), dict)
+
+            self.assertEqual(data['status'], 'OK')
+            self.assertEqual(data1.json()['status'], 'OK')
+            self.assertEqual(data2.json()['status'], 'OK')
+
+            self.assertTrue(data == data1.json() == data2.json())
+
+        # Testing without Context Manager
+        client = polygon.PolygonClient(cred.KEY)
+        data = client.get_daily_open_close('AMD', '2021-06-28')
+        client.close()
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data['status'], 'OK')
+
+    def test_get_aggregate_bars(self):
+        with polygon.PolygonClient(cred.KEY) as client:
+            data = client.get_aggregate_bars('NVDA', '2020-06-28', '2021-06-28', limit=10)
+            data1 = client.get_aggregate_bars('NVDA', '2020-06-28', datetime.date(2021, 6, 28), limit=10,
+                                              raw_response=True)
+            data2 = client.get_aggregate_bars('NVDA', '2020-06-28', datetime.date(2021, 6, 28), limit=10,
+                                              raw_response=True, timespan='minute', multiplier=5)
+            data3 = client.get_aggregate_bars('NVDA', datetime.date(2020, 6, 28), '2021-06-28', limit=10,
+                                              raw_response=False, timespan='minute', multiplier=1)
+
+            self.assertIsInstance(data, dict)
+            self.assertIsInstance(data3, dict)
+            self.assertIsInstance(data1, Response)
+            self.assertIsInstance(data2, Response)
+
+            self.assertIsInstance(data1.json(), dict)
+            self.assertIsInstance(data2.json(), dict)
+
+            self.assertLessEqual(len(data['results']), 10)
+            self.assertLessEqual(len(data1.json()['results']), 10)
+            self.assertLessEqual(len(data2.json()['results']), 10)
+            self.assertLessEqual(len(data3['results']), 10)
+
+        # Testing without Context Manager
+        client = polygon.PolygonClient(cred.KEY)
+        data = client.get_aggregate_bars('NVDA', '2020-06-28', '2021-06-28', limit=10)
+        client.close()
+        self.assertIsInstance(data, dict)
+        self.assertLessEqual(len(data['results']), 10)
+
     @async_test
     async def test_async_get_trades(self):
         async with polygon.PolygonClient(cred.KEY, True) as client:
@@ -200,6 +281,90 @@ class TestStocks(unittest.TestCase):
         await client.async_close()
         self.assertIsInstance(data, dict)
         self.assertEqual(data['status'], 'OK')
+
+    @async_test
+    async def test_async_get_last_quote(self):
+        async with polygon.PolygonClient(cred.KEY, True) as client:
+            data = await client.async_get_last_quote('AMD')
+            data1 = await client.async_get_last_quote('AMD', raw_response=True)
+            data2 = await client.async_get_last_quote('AMD', raw_response=True)
+
+            self.assertIsInstance(data, dict)
+            self.assertIsInstance(data1, HttpxResponse)
+            self.assertIsInstance(data2, HttpxResponse)
+
+            self.assertIsInstance(data1.json(), dict)
+            self.assertIsInstance(data2.json(), dict)
+
+            self.assertEqual(data['status'], 'OK')
+            self.assertEqual(data1.json()['status'], 'OK')
+            self.assertEqual(data2.json()['status'], 'OK')
+
+        # Testing without Context Manager
+        client = polygon.PolygonClient(cred.KEY, True)
+        data = await client.async_get_last_quote('AMD')
+        await client.async_close()
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data['status'], 'OK')
+
+    @async_test
+    async def test_async_get_daily_open_close(self):
+        async with polygon.PolygonClient(cred.KEY, True) as client:
+            data = await client.async_get_daily_open_close('AMD', '2021-06-28')
+            data1 = await client.async_get_daily_open_close('AMD', datetime.datetime(2021, 6, 28), raw_response=True)
+            data2 = await client.async_get_daily_open_close('AMD', datetime.datetime(2021, 6, 28), raw_response=True,
+                                                            adjusted=False)
+
+            self.assertIsInstance(data, dict)
+            self.assertIsInstance(data1, HttpxResponse)
+            self.assertIsInstance(data2, HttpxResponse)
+
+            self.assertIsInstance(data1.json(), dict)
+            self.assertIsInstance(data2.json(), dict)
+
+            self.assertEqual(data['status'], 'OK')
+            self.assertEqual(data1.json()['status'], 'OK')
+            self.assertEqual(data2.json()['status'], 'OK')
+
+            self.assertTrue(data == data1.json() == data2.json())
+
+        # Testing without Context Manager
+        client = polygon.PolygonClient(cred.KEY, True)
+        data = await client.async_get_daily_open_close('AMD', '2021-06-28')
+        await client.async_close()
+        self.assertIsInstance(data, dict)
+        self.assertEqual(data['status'], 'OK')
+
+    @async_test
+    async def test_get_aggregate_bars(self):
+        async with polygon.PolygonClient(cred.KEY, True) as client:
+            data = await client.async_get_aggregate_bars('NVDA', '2020-06-28', '2021-06-28', limit=10)
+            data1 = await client.async_get_aggregate_bars('NVDA', '2020-06-28', datetime.date(2021, 6, 28), limit=10,
+                                                          raw_response=True)
+            data2 = await client.async_get_aggregate_bars('NVDA', '2020-06-28', datetime.date(2021, 6, 28), limit=10,
+                                                          raw_response=True, timespan='minute', multiplier=5)
+            data3 = await client.async_get_aggregate_bars('NVDA', datetime.date(2020, 6, 28), '2021-06-28', limit=10,
+                                                          raw_response=False, timespan='minute', multiplier=1)
+
+            self.assertIsInstance(data, dict)
+            self.assertIsInstance(data3, dict)
+            self.assertIsInstance(data1, HttpxResponse)
+            self.assertIsInstance(data2, HttpxResponse)
+
+            self.assertIsInstance(data1.json(), dict)
+            self.assertIsInstance(data2.json(), dict)
+
+            self.assertLessEqual(len(data['results']), 10)
+            self.assertLessEqual(len(data1.json()['results']), 10)
+            self.assertLessEqual(len(data2.json()['results']), 10)
+            self.assertLessEqual(len(data3['results']), 10)
+
+        # Testing without Context Manager
+        client = polygon.PolygonClient(cred.KEY, True)
+        data = await client.async_get_aggregate_bars('NVDA', '2020-06-28', '2021-06-28', limit=10)
+        await client.async_close()
+        self.assertIsInstance(data, dict)
+        self.assertLessEqual(len(data['results']), 10)
 
 
 # ========================================================= #

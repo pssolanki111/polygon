f630866d7aad6c91465c28d5ab03d582de8fc0e1
P S Solanki <pssolanki111pss@gmail.com>
2021-10-13 20:22:49 +0530

    changed version num for docs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as file:
 
 setup(
     name='polygon',
-    version='0.5.0',
+    version='0.5.1',
     packages=['polygon', 'polygon.forex', 'polygon.crypto', 'polygon.stocks', 'polygon.streaming',
               'polygon.reference_apis', 'polygon.options'],
     url='https://github.com/pssolanki111/polygon',

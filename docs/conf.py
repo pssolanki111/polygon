4c32c948a683dc31b2bf3d10fef94c008b682ba9
P S Solanki <pssolanki111pss@gmail.com>
2021-10-14 18:08:22 +0530

    only pushing to change docs version num for major release. see last commit for changes

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, P S Solanki'
 author = 'P S Solanki'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.1'
+release = '0.5.5'
 
 
 # -- General configuration ---------------------------------------------------
